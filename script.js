26b5d06c4dbf3484e7155976230a0f6ef07bdbc3
joeyjet625 <44040581+joeyjet625@users.noreply.github.com>
2018-12-12 17:45:35 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".superman").click(function(){
+    $(".superman2").toggle();
+});
+
+$(".spiderman2").click(function(){
+    $(".spiderman").fadeToggle();
+});
+
+$(".hulk2").click(function(){
+    $(".hulk").slideToggle();
+});
